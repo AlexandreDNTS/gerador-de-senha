0d7ddd1d3b3e997a3edc37c95ca5ea0f710720f9
Francimar Alexandre <92753716+AlexandreDNTS@users.noreply.github.com>
2022-11-25 14:19:05 -0300

    iniciando as validações

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import PySimpleGUI as sg
+from random import randint
 
 
 def tela_inicial():
     layout = [
         [sg.Text('\n\tGERADOR DE SENHA\t')],
         [sg.Text('tamanho da senha')],
-        [sg.Text('', key='msgtam')],
         [sg.Input(key='tam')],
+        [sg.Text('', key='msgtam')],
         [sg.Button('gerar senha')],
         [sg.Text('', key='senha')]
     ]
@@ -19,3 +20,14 @@ while True:
     window, eventos, valores = sg.read_all_windows()
     if eventos == sg.WIN_CLOSED:
         break
+    if window == telaInicial and eventos == 'gerar senha':
+        valor = int(valores['tam'])
+        if valor >= 5:
+            TAM = valor
+            for i in range(0, TAM):
+                s = randint(0, TAM)
+
+                window['senha'].update(f'{s}')
+
+        else:
+            window['msgtam'].update('tamanho minimo 5')
