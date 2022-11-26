887b4581fdc6498bbe74dc09a6f1a75b9ef9e898
Francimar Alexandre <92753716+AlexandreDNTS@users.noreply.github.com>
2022-11-25 21:52:01 -0300

    corrigindo bug  valor tamanho da senha vazio

--- main.py
+++ main.py
@@ -22,15 +22,20 @@ while True:
     window, eventos, valores = sg.read_all_windows()
     if eventos == sg.WIN_CLOSED:
         break
+
     if window == telaInicial and eventos == 'gerar senha':
-        valor = int(valores['tam'])
-        if valor < 5 or valor > 25:
-            window['senha'].update(
-                'a senha deve ter um tamanho minimo de  5 caractere e um tamanho maximo de 25 caracteres')
+        window['senha'].update('')
+        if valores['tam'] != '':
+            valor = int(valores['tam'])
+            if valor < 5 or valor > 25:
+                window['senha'].update(
+                    'a senha deve ter um tamanho minimo de  5 caractere e um tamanho maximo de 25 caracteres')
+            else:
+                i = 0
+                while i < valor:
+                    senha = randint(0, valor)
+                    listasenha.append(senha)
+                    i += 1
+                window['msgtam'].update(listasenha)
         else:
-            i = 0
-            while i < valor:
-                senha = randint(0, valor)
-                listasenha.append(senha)
-                i += 1
-            window['msgtam'].update(listasenha)
+            window['msgtam'].update('digite um tamnho para senha')
